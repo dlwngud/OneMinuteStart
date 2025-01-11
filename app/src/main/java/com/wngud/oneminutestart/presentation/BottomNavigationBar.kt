3d5e3dfeadfeafa108263af47014426a21e60af2
이주형 <leewngud980411@gmail.com>
2025-01-11 22:44:52 +0900

    [feat] NavigationBar 아이콘, 글자색 - 다크모드와 라이트모드 적용 - indicatorColor 투명하게 적용

--- app/src/main/java/com/wngud/oneminutestart/presentation/BottomNavigationBar.kt
+++ app/src/main/java/com/wngud/oneminutestart/presentation/BottomNavigationBar.kt
@@ -1,13 +1,18 @@
 package com.wngud.oneminutestart.presentation
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.sp
@@ -22,6 +27,7 @@ fun BottomNavigationBar(
 ) {
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
+    val isDarkTheme = isSystemInDarkTheme()
 
     val bottomScreens = listOf(
         Screen.TimerScreen,
@@ -44,15 +50,30 @@ fun BottomNavigationBar(
                             text = item.name,
                             style = TextStyle(
                                 fontSize = 12.sp
-                            )
+                            ),
+                            color = if (currentRoute == item.route) {
+                                if(isDarkTheme) Color.White
+                                else Color.Black
+                            } else {
+                                Color.Gray
+                            }
                         )
                     },
                     icon = {
                         Icon(
                             painter = painterResource(id = item.icon),
-                            contentDescription = item.name
+                            contentDescription = item.name,
+                            tint = if (currentRoute == item.route) {
+                                if(isDarkTheme) Color.White
+                                else Color.Black
+                            } else {
+                                Color.Gray
+                            }
                         )
                     },
+                    colors = NavigationBarItemDefaults.colors(
+                        indicatorColor = Color.Transparent
+                    ),
                     onClick = {
                         navController.navigate(item.route) {
                             launchSingleTop = true
