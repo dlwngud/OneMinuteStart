331eaf80c6910ade43b641d324e004aa54b67845
이주형 <leewngud980411@gmail.com>
2025-01-11 22:04:54 +0900

    [feat] AppBar composable 함수 생성

---
+++ app/src/main/java/com/wngud/oneminutestart/presentation/AppBar.kt
@@ -0,0 +1,50 @@
+package com.wngud.oneminutestart.presentation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppBar(
+    title: String,
+    hasBackButton: Boolean,
+    onBackNavClicked: () -> Unit = {}
+) {
+    if(hasBackButton) {
+        val navigationIcon: @Composable () -> Unit = {
+            IconButton(onClick = { onBackNavClicked() }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = null
+                )
+            }
+        }
+        TopAppBar(
+            title = {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Bold
+                )
+            },
+            navigationIcon = navigationIcon
+        )
+    } else {
+        TopAppBar(
+            title = {
+                Text(
+                    text = title,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        )
+    }
+
+}
