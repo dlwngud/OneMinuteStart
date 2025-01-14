a84229c0831ca07d6ad678f3d34564a470d3e3a4
이주형 <leewngud980411@gmail.com>
2025-01-14 22:21:13 +0900

    [feat] 추가º수정, 삭제 다이얼로그

--- app/src/main/java/com/wngud/oneminutestart/presentation/timer/TimerScreen.kt
+++ app/src/main/java/com/wngud/oneminutestart/presentation/timer/TimerScreen.kt
@@ -3,7 +3,9 @@ package com.wngud.oneminutestart.presentation.timer
 import android.annotation.SuppressLint
 import androidx.activity.compose.BackHandler
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +18,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
@@ -31,14 +34,19 @@ import androidx.compose.material.rememberSwipeableState
 import androidx.compose.material.swipeable
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -48,19 +56,24 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavHostController
 import com.wngud.oneminutestart.domain.Task
-import com.wngud.oneminutestart.presentation.AppBar
+import com.wngud.oneminutestart.presentation.components.AMPMPicker
+import com.wngud.oneminutestart.presentation.components.AppBar
+import com.wngud.oneminutestart.presentation.components.NumberPicker
 import kotlinx.coroutines.launch
 
 @Composable
@@ -71,6 +84,8 @@ fun TimerScreen(
     val coroutineScope = rememberCoroutineScope()
     val tabs = listOf("1분만 해보자!", "더 해볼래?")
     val pagerState = rememberPagerState(pageCount = { 2 })
+    var showTaskDialog by remember { mutableStateOf(false) }
+    var showDeleteDialog by remember { mutableStateOf(false) }
 
     val dummyTasks = listOf(
         Task(
@@ -99,6 +114,25 @@ fun TimerScreen(
         )
     )
     val dummyEmptyList = listOf<Task>()
+
+    if (showTaskDialog) {
+        TaskDialog(
+            task = null,
+            title = "수정",
+            onDismissDialog = {
+                showTaskDialog = false
+            }
+        )
+    }
+
+    if (showDeleteDialog) {
+        DeleteDialog(
+            task = dummyTasks[0],
+            onDismissDialog = {
+                showDeleteDialog = false
+            }
+        )
+    }
 
     BackHandler {
         onBackPressed()
@@ -154,7 +188,11 @@ fun TimerScreen(
                                 "작은 시작이 큰 변화를 만들어낼 수 있어요",
                         buttonText = "일단 시작!",
                         onButtonClick = { },
-                        page = page
+                        page = page,
+                        onDialogRequested = { showTaskDialog = true },
+                        onDismissDialog = { showTaskDialog = false },
+                        onDeleteDialogRequested = { showDeleteDialog = true },
+                        onDismissDeleteDialog = { showDeleteDialog = false }
                     )
 
                     1 -> TaskListTab(
@@ -164,7 +202,11 @@ fun TimerScreen(
                                 "그 작은 시작이 여러분을 더 많은 작업으로 이끌어줄 거예요",
                         buttonText = "더 해볼래!",
                         onButtonClick = { },
-                        page = page
+                        page = page,
+                        onDialogRequested = { showTaskDialog = true },
+                        onDismissDialog = { showTaskDialog = false },
+                        onDeleteDialogRequested = { showDeleteDialog = true },
+                        onDismissDeleteDialog = { showDeleteDialog = false }
                     )
                 }
             }
@@ -178,7 +220,11 @@ fun TaskListTab(
     emptyMessage: String,
     buttonText: String,
     page: Int,
-    onButtonClick: (Task) -> Unit
+    onButtonClick: (Task) -> Unit,
+    onDialogRequested: () -> Unit,
+    onDismissDialog: () -> Unit,
+    onDeleteDialogRequested: () -> Unit,
+    onDismissDeleteDialog: () -> Unit
 ) {
     var itemHeight by remember { mutableStateOf(0.dp) }
     val density = LocalDensity.current
@@ -204,7 +250,9 @@ fun TaskListTab(
                             .fillMaxWidth()
                             .padding(horizontal = 16.dp, vertical = 8.dp),
                         shape = RoundedCornerShape(16.dp),
-                        onClick = {}
+                        onClick = {
+                            onDialogRequested()
+                        }
                     ) {
                         Row(
                             modifier = Modifier
@@ -252,7 +300,11 @@ fun TaskListTab(
                     isSelected = selectedTaskId == task.id,
                     onSelect = { isSelected ->
                         selectedTaskId = if (isSelected) task.id else null
-                    }
+                    },
+                    onDialogRequested = onDialogRequested,
+                    onDismissDialog = onDismissDialog,
+                    onDeleteDialogRequested = onDeleteDialogRequested,
+                    onDismissDeleteDialog = onDismissDeleteDialog
                 )
             }
             if (page == 0) {
@@ -263,7 +315,9 @@ fun TaskListTab(
                             .padding(horizontal = 16.dp, vertical = 8.dp)
                             .height(itemHeight),
                         shape = RoundedCornerShape(16.dp),
-                        onClick = {}
+                        onClick = {
+                            onDialogRequested()
+                        }
                     ) {
                         Row(
                             modifier = Modifier
@@ -296,7 +350,11 @@ fun SwipeTask(
     onItemHeightChange: (IntSize) -> Unit,
     height: Dp,
     isSelected: Boolean,
-    onSelect: (Boolean) -> Unit
+    onSelect: (Boolean) -> Unit,
+    onDialogRequested: () -> Unit,
+    onDismissDialog: () -> Unit,
+    onDeleteDialogRequested: () -> Unit,
+    onDismissDeleteDialog: () -> Unit
 ) {
     val swipeableState = rememberSwipeableState(initialValue = 0)
     val coroutineScope = rememberCoroutineScope()
@@ -323,9 +381,6 @@ fun SwipeTask(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .onGloballyPositioned { coordinates ->
-                onItemHeightChange(coordinates.size)
-            }
             .swipeable(
                 state = swipeableState,
                 orientation = Orientation.Horizontal,
@@ -344,6 +399,7 @@ fun SwipeTask(
                     .height(height)
                     .padding(horizontal = 16.dp),
                 onClick = {
+                    onDialogRequested()
                     coroutineScope.launch {
                         swipeableState.animateTo(0, tween(600, 0))
                     }
@@ -370,12 +426,13 @@ fun SwipeTask(
                     .height(height)
                     .padding(horizontal = 16.dp),
                 onClick = {
+                    onDeleteDialogRequested()
                     coroutineScope.launch {
                         swipeableState.animateTo(0, tween(600, 0))
                     }
                 },
                 colors = ButtonColors(
-                    Color(0xFFfe3130),
+                    Color(0xFFDB4455),
                     contentColor = Color.White,
                     disabledContainerColor = Color.Transparent,
                     disabledContentColor = Color.Transparent,
@@ -439,57 +496,209 @@ fun SwipeTask(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TaskItemWithButton(
-    task: Task,
-    buttonText: String,
-    onClick: () -> Unit,
-    onItemHeightChange: (IntSize) -> Unit
+fun TaskDialog(
+    task: Task?,
+    title: String,
+    onDismissDialog: () -> Unit
 ) {
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 8.dp, horizontal = 16.dp)
-            .onGloballyPositioned { coordinates ->
-                onItemHeightChange(coordinates.size)
-            },
-        shape = RoundedCornerShape(16.dp)
-    ) {
-        Row(
+    Dialog(onDismissRequest = { onDismissDialog() }) {
+        Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
+                .wrapContentHeight()
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp),
         ) {
             Column(
                 modifier = Modifier
-                    .padding(start = 8.dp)
-                    .weight(1f)
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Text(
-                    text = task.title,
-                    style = MaterialTheme.typography.titleMedium.copy(fontWeight = FontWeight.Bold)
+                    text = title,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
                 )
-                if (!task.reminderTime.isNullOrEmpty()) {
-                    Text(
-                        text = "리마인드 알림: ${task.reminderTime}",
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.secondary
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "작업 이름",
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(horizontal = 16.dp)
+                )
+                OutlinedTextField(
+                    value = "dd",
+                    onValueChange = { },
+                    label = {
+                        Text(
+                            "작업 이름",
+                            fontWeight = FontWeight.Thin
+                        )
+                    },
+                    maxLines = 1,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clip(RoundedCornerShape(16.dp))
+                        .padding(horizontal = 16.dp),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.LightGray,
+                        unfocusedBorderColor = Color.LightGray
                     )
-                } else {
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Row(
+                    modifier = Modifier.padding(horizontal = 16.dp)
+                ) {
                     Text(
-                        text = "리마인드 알림없음",
-                        style = MaterialTheme.typography.bodySmall,
-                        color = MaterialTheme.colorScheme.secondary
+                        text = "리마인드 알람",
+                        modifier = Modifier.weight(1f)
+                    )
+                    Switch(
+                        checked = true,
+                        onCheckedChange = {}
                     )
                 }
-            }
-            Button(
-                onClick = onClick,
-                shape = RoundedCornerShape(16.dp)
-            ) {
-                Text(text = buttonText)
+
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    AMPMPicker(AMPM_LIST) { }
+                    Row {
+                        NumberPicker(numbers = HOUR_LIST) {
+
+                        }
+                        Text(text = "시", modifier = Modifier.align(Alignment.CenterVertically))
+                    }
+
+                    Row {
+                        NumberPicker(numbers = MINUTE_LIST) {
+
+                        }
+                        Text(text = "분", modifier = Modifier.align(Alignment.CenterVertically))
+                    }
+                }
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 16.dp),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Gray
+                        ),
+                        shape = RoundedCornerShape(16.dp),
+                        onClick = {
+                            onDismissDialog()
+                        }
+                    ) {
+                        Text("취소")
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary
+                        ),
+                        shape = RoundedCornerShape(16.dp),
+                        onClick = {
+                            onDismissDialog()
+                        }
+                    ) {
+                        Text("추가")
+                    }
+                }
             }
         }
     }
 }
+
+@Composable
+fun DeleteDialog(
+    task: Task,
+    onDismissDialog: () -> Unit
+) {
+    val isDarkMode = isSystemInDarkTheme()
+
+    Dialog(onDismissRequest = { onDismissDialog() }) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(16.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = MaterialTheme.colorScheme.background)
+                    .padding(top = 16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "작업을 삭제할까요?",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = Color(0xFFDB4455),
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = "삭제한 작업은 복구할 수 없어요.",
+                    color = Color.Gray,
+                    fontSize = 14.sp
+                )
+                Spacer(modifier = Modifier.height(32.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp, vertical = 16.dp),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Button(
+                        onClick = {
+                            onDismissDialog()
+                        },
+                        modifier = Modifier.weight(1f),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color.Gray
+                        ),
+                        shape = RoundedCornerShape(16.dp),
+                    ) {
+                        Text(
+                            text = "취소",
+                            color = if (isDarkMode) Color.White else Color.Unspecified
+                        )
+                    }
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Button(
+                        modifier = Modifier.weight(1f),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFFDB4455)
+                        ),
+                        shape = RoundedCornerShape(16.dp),
+                        onClick = {
+                            onDismissDialog()
+                        }
+                    ) {
+                        Text(
+                            text = "삭제하기",
+                            color = if (isDarkMode) Color.White else Color.Unspecified
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+val HOUR_LIST = (-1..13).toList()
+val MINUTE_LIST = (-1..60).toList()
+val AMPM_LIST = listOf("", "오전", "오후", "")
