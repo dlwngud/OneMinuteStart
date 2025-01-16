1c8a34e423fc97f457ea5a511e1f39c1f8fbce8c
이주형 <leewngud980411@gmail.com>
2025-01-16 22:11:03 +0900

    [feat] 1분만 화면 Scaffold 추가

--- app/src/main/java/com/wngud/oneminutestart/presentation/timer/OneMinuteScreen.kt
+++ app/src/main/java/com/wngud/oneminutestart/presentation/timer/OneMinuteScreen.kt
@@ -1,12 +1,16 @@
 package com.wngud.oneminutestart.presentation.timer
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.wngud.oneminutestart.presentation.components.AppBar
 import com.wngud.oneminutestart.presentation.components.CircularCountDownTimer
 
 @Composable
@@ -14,9 +18,25 @@ fun OneMinuteScreen(
     navController: NavHostController,
     id: Long
 ) {
-    Column(
-        modifier = Modifier.fillMaxWidth().padding(16.dp)
-    ) {
-        CircularCountDownTimer("운동하기")
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            AppBar(
+                title = "",
+                hasBackButton = true,
+                onBackNavClicked = {
+                    navController.navigateUp()
+                })
+        },
+        containerColor = MaterialTheme.colorScheme.background
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            CircularCountDownTimer("운동하기")
+        }
     }
 }
