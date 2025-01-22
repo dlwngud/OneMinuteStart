8ba6f95d8debe5c1dbd0665bcdc826ede29595d0
이주형 <leewngud980411@gmail.com>
2025-01-22 23:54:20 +0900

    [feat] TimeViewModel 구현

--- app/src/main/java/com/wngud/oneminutestart/domain/Task.kt
+++ app/src/main/java/com/wngud/oneminutestart/domain/Task.kt
@@ -1,11 +1,16 @@
 package com.wngud.oneminutestart.domain
 
-import java.time.LocalDate
-
+import com.wngud.oneminutestart.data.db.local.TaskEntity
 data class Task(
     val id: Long,                    // 고유 ID
     val title: String,              // 작업 제목
     val reminderTime: String?,      // 알림 시간 (HH:mm 포맷)
     val isCompletedOneMinute: Boolean = false, // 완료 여부
-    val completedDate: LocalDate? = null // 완료 날짜 (선택 사항)
+)
+
+fun Task.toTaskEntity(): TaskEntity = TaskEntity(
+    id = this.id,
+    title = this.title,
+    reminderTime = this.reminderTime,
+    isCompletedOneMinute = this.isCompletedOneMinute
 )
