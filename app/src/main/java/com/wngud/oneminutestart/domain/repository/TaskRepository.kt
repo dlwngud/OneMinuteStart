744e9994ec291d4ef27eff4d1860ec37b10204e9
이주형 <leewngud980411@gmail.com>
2025-01-22 22:54:45 +0900

    [feat] task repository

---
+++ app/src/main/java/com/wngud/oneminutestart/domain/repository/TaskRepository.kt
@@ -0,0 +1,9 @@
+package com.wngud.oneminutestart.domain.repository
+
+import com.wngud.oneminutestart.domain.Task
+import kotlinx.coroutines.flow.Flow
+
+interface TaskRepository {
+
+    fun getAllTasks(): Flow<List<Task>>
+}
