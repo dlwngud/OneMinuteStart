5b421030a129293a85d39c666284e4a854a26554
이주형 <leewngud980411@gmail.com>
2025-01-22 22:54:45 +0900

    [feat] task repository

---
+++ app/src/main/java/com/wngud/oneminutestart/data/repository/TaskRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.wngud.oneminutestart.data.repository
+
+import com.wngud.oneminutestart.data.db.local.TaskDao
+import com.wngud.oneminutestart.data.db.local.toTask
+import com.wngud.oneminutestart.domain.Task
+import com.wngud.oneminutestart.domain.repository.TaskRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class TaskRepositoryImpl @Inject constructor(
+    private val taskDao: TaskDao
+) : TaskRepository {
+
+    override fun getAllTasks(): Flow<List<Task>> =
+        taskDao.getAllTasks().map { it.map { it.toTask() } }
+
+}
